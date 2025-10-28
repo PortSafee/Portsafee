1c24bd7f44d4ab4bfe706670919741ef61c62884
Pedro <pedrohenriquedias966@icloud.com>
2025-10-28 19:26:56 -0300

    :sparkles: feat: News Controllers

--- CondominioEntregas.API/Program.cs
+++ CondominioEntregas.API/Program.cs
@@ -1,17 +1,22 @@
 using Microsoft.OpenApi.Models;
+using PortSafe.Data;
+using Microsoft.EntityFrameworkCore;
+using PortSafe.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-
+builder.Services.AddDbContext<PortSafeContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))); // Configura o DbContext com PostgreSQL
 
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<AuthService>();
+
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minha API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PortSafe", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
@@ -38,6 +43,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -45,7 +52,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
-        c.RoutePrefix = string.Empty; // Makes Swagger UI available at the root
+        c.RoutePrefix = string.Empty; // Define a raiz do Swagger UI
     });
 }
 
